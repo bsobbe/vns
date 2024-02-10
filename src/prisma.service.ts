70e6a7891cd2572aedb023c371dbddaab9198b9c
Sam Bagheri <hi@thedevway.com>
2024-02-10 17:56:27 +0100

    Error handling for db events

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,15 +1,35 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly logger: PinoLogger) {
+    super();
+    this.logger.setContext(PrismaService.name);
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (e) {
+      this.logger.fatal(
+        'onModuleInit() Database connection failed. Do not panic!',
+        e,
+      );
+    }
   }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
-      await app.close();
+      try {
+        await app.close();
+      } catch (e) {
+        this.logger.warn(
+          'enableShutdownHooks() Database connection failed to close cleanly.',
+          e,
+        );
+      }
     });
   }
 }
