d9a31077aea938ac4e69823513fbdbc0dfaea309
Sam Bagheri <hi@thedevway.com>
2024-02-11 16:01:03 +0100

    Updating comments

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Runs migration for dev on startup.
 npx prisma migrate dev
-# Runs the application in dev mode.
+
+# Runs the application in watch mode.
 npm run start:dev
+# Runs the application in dev mode.
+npm run start
